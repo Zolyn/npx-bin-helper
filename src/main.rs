6dbf1082b0a047c5d62a9505f1b46a92616e6578
Yumeoto Zorin <zorin1477@gmail.com>
2022-11-04 01:41:56 +0800

    chore: update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::process;
 
 fn main() {
+    env_logger::init();
+
     let result = npx_bin_helper::gen_command().unwrap_or_else(|e| {
         eprintln!("[NPX_BIN_HELPER]: An error was occurred: {}", e);
         process::exit(1)
     });
+
     print!("{}", result);
 }
