2e4b4fdb33f092f9fc85d39f16dcadbda3f50bd8
zorin1477@gmail.com <zorin1477@gmail.com>
2022-11-11 00:06:06 +0800

    feat: support powershell

--- src/shells/powershell.rs
+++ src/shells/powershell.rs
@@ -17,6 +17,30 @@ impl Shell for PowerShell {
     }
     fn gen_setup_script(&self) -> &'static str {
         r#"
+function __npx_bin_helper_pwd {
+    $cwd = Get-Location
+    if ($cwd.Provider.Name -eq "FileSystem") {
+        $cwd.ProviderPath
+    }
+}
+
+function __npx_bin_helper {
+    $result = __npx_bin_helper_pwd
+    if ($null -ne $result) {
+        npx-bin-helper -s powershell | Out-String | Invoke-Expression
+    }
+}
+
+$__npx_bin_helper_hooked = (Get-Variable __npx_bin_helper_hooked -ValueOnly -ErrorAction SilentlyContinue)
+
+if ($__npx_bin_helper_hooked -ne 1) {
+    $__npx_bin_helper_hooked = 1
+    $prompt_old = $function:prompt
+    function prompt {
+        $null = __npx_bin_helper
+        & $prompt_old
+    }
+}
 "#
     }
 }
